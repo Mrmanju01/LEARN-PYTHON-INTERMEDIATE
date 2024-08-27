2942c979ae7a7a899dbc4d9ffb3ac7a749cacbe7
K.Manjunathareddy <134767789+Mrmanju01@users.noreply.github.com>
2024-08-27 19:37:26 +0530

    Encapsulation.py

--- Encapsulation.py
+++ Encapsulation.py
@@ -22,3 +22,23 @@ sal.display_info()
 print(sal.get_salary())
 sal.set_salary(15000)
 sal.display_info()
+***************************************
+class Account:
+    def __init__(self, balance):
+        self.__balance = balance  # Private attribute
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.__balance += amount
+
+    def withdraw(self, amount):
+        if 0 < amount <= self.__balance:
+            self.__balance -= amount
+
+    def get_balance(self):
+        return self.__balance
+
+acc = Account(1000)
+acc.deposit(500)
+print(acc.get_balance())  # Output: 1500
+
